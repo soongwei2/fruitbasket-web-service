718a7739f01badfa6505e40c94ea261ccdfa10f6
soongwei.wong@tanchonggroup.com <soongwei.wong@tanchonggroup.com>
2020-05-09 13:50:07 +0800

    filter expired promos

--- services/product/functions.js
+++ services/product/functions.js
@@ -1,40 +1,43 @@
 const _ = require('lodash');
 const fruitBasketMaster = require('@soongwei/mysql-db/databases/fruitBasketMaster');
-const Sequelize = fruitBasketMaster.sequelize;
+const sequelize = fruitBasketMaster.sequelize;
+const Sequelize = sequelize.Sequelize;
 
 class Functions {
-    static getProduct(payload){
-         return fruitBasketMaster.product.findAll({
-            where: payload,
-            include: [   
-                {
+    static getProduct(payload) {
+        return fruitBasketMaster.product.findAll({
+                where: payload,
+                include: [{
                     model: fruitBasketMaster.promotionRules,
-                    include:[
-                        {
-                            model: fruitBasketMaster.promotion,
-                            include:[
-                                {   
-                                    model: fruitBasketMaster.promotionRules
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ]
-        })
-        .map(el => el.get({ plain: true }))
-        .then((results) => {
-            //get promotion and flatten it
-            return results.map((eachResult) => {
-                const allPromos = eachResult.promotionRules.map((eachPromoRules) => {
-                    return eachPromoRules.promotion
-                })
-                delete eachResult.promotionRules;
-                eachResult.promotion = _.uniqBy(allPromos, 'id');
-                return eachResult;
-            });
-        })
-    } 
+                    include: [{
+                        model: fruitBasketMaster.promotion,
+                        where: {
+                            expiryDate: {
+                                [Sequelize.Op.gt]: Sequelize.literal('CURRENT_TIMESTAMP'),
+                            },
+                        },
+                        include: [{
+                            model: fruitBasketMaster.promotionRules,
+                          
+                        }]
+                    }]
+                }]
+            })
+            .map(el => el.get({
+                plain: true
+            }))
+            .then((results) => {
+                //get promotion and flatten it
+                return results.map((eachResult) => {
+                    const allPromos = eachResult.promotionRules.map((eachPromoRules) => {
+                        return eachPromoRules.promotion
+                    })
+                    delete eachResult.promotionRules;
+                    eachResult.promotion = _.uniqBy(allPromos, 'id');
+                    return eachResult;
+                });
+            })
+    }
 }
 
 module.exports = Functions;
